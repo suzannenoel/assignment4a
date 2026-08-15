c6f0ca5e9df25ae21d87078f4909b7022fcbf3f3
suzannenoel <158251037+suzannenoel@users.noreply.github.com>
2026-08-15 16:25:38 -0600

    Update ui.js

--- assign4/assignment3/materialize/js/ui.js
+++ assign4/assignment3/materialize/js/ui.js
@@ -14,12 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
     checkStorageUsage();
   });
    //
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/serviceworker.js")
-      .then((req) => console.log("Service Worker Registered!", req))
-      .catch((err) => console.log("Service Worker registration failed", err));
-  }
+ 
    //
   //create indexedDB database
   async function createDB(){
