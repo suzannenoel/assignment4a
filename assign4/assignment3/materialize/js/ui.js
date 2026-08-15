43fa956d735d0c3bee4f609392c7b6a27c078166
suzannenoel <158251037+suzannenoel@users.noreply.github.com>
2026-08-15 16:57:20 -0600

    Update ui.js

--- assign4/assignment3/materialize/js/ui.js
+++ assign4/assignment3/materialize/js/ui.js
@@ -82,7 +82,7 @@ if (!id){
    await store.put({ ...task, id: taskId, synced: true });   
    await tx.done;
   }else {
-    taskId ='temp-${Date.now()}';
+    taskId =`temp-${Date.now()}`;
     console.log(taskId);
    const taskToStore = { ...task, taskId, synced: false};
   (!taskToStore.id){
