b8820ea9a0ea3b2ad2b036ec073c919ef4645f6c
suzannenoel <158251037+suzannenoel@users.noreply.github.com>
2026-08-15 21:21:06 -0600

    Update firebaseDB.js

--- assign4/assignment3/materialize/js/firebaseDB.js
+++ assign4/assignment3/materialize/js/firebaseDB.js
@@ -2,7 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-  import { getFirestore,collection,doc,addDoc,getDocs,deleteDoc,updateDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+  import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, updateDoc, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
